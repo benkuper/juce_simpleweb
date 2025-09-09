daf6b306fc33a239c4bafc7c72d93415f85ba8a2
Ben Kuper <bkuperberg@gmail.com>
2025-09-09 11:36:33 +0200

    fix potential out-of-root exploit

--- SimpleWebSocketServer.cpp
+++ SimpleWebSocketServer.cpp
@@ -13,14 +13,7 @@
 
 using namespace juce;
 
-SimpleWebSocketServerBase::SimpleWebSocketServerBase() :
-	Thread("Web socket"),
-	port(0),
-	allowAddressReuse(false),
-	isConnected(false)
-{
-
-}
+SimpleWebSocketServerBase::SimpleWebSocketServerBase() : Thread("Web socket"), port(0), allowAddressReuse(false), isConnected(false) {}
 
 SimpleWebSocketServerBase::~SimpleWebSocketServerBase()
 {
@@ -40,24 +33,22 @@ void SimpleWebSocketServerBase::start(int _port, const String& _wsSuffix, const 
 
 void SimpleWebSocketServerBase::send(const MemoryBlock& data)
 {
-	send((const char*)data.getData(), (int)data.getSize());
+	send((const char*) data.getData(), (int) data.getSize());
 }
 
 void SimpleWebSocketServerBase::stop()
 {
-
-	//#if !JUCE_DEBUG
+	// #if !JUCE_DEBUG
 	//	if (Thread::getCurrentThreadId() != this->getThreadId()) stopThread(500);
-	//#endif
+	// #endif
 
 	stopInternal();
 	isConnecting = false;
 	isConnected = false;
 
-	//#if JUCE_DEBUG //don't know why the order is not the same for debug and release...
+	// #if JUCE_DEBUG //don't know why the order is not the same for debug and release...
 	//	if (Thread::getCurrentThreadId() != this->getThreadId()) stopThread(500);
-	//#endif
-
+	// #endif
 }
 
 void SimpleWebSocketServerBase::closeConnection(const String& id, int code, const String& reason)
@@ -67,7 +58,7 @@ void SimpleWebSocketServerBase::closeConnection(const String& id, int code, cons
 
 void SimpleWebSocketServerBase::run()
 {
-	//HTTP init
+	// HTTP init
 	isConnected = false;
 	isConnecting = true;
 	initServer();
@@ -83,12 +74,9 @@ void SimpleWebSocketServerBase::removeHTTPRequestHandler(RequestHandler* handler
 	handlers.removeAllInstancesOf(handlerToRemove);
 }
 
-//SIMPLE
+// SIMPLE
 
-
-SimpleWebSocketServer::SimpleWebSocketServer()
-{
-}
+SimpleWebSocketServer::SimpleWebSocketServer() {}
 
 SimpleWebSocketServer::~SimpleWebSocketServer()
 {
@@ -98,7 +86,10 @@ SimpleWebSocketServer::~SimpleWebSocketServer()
 void SimpleWebSocketServer::send(const String& message)
 {
 	HashMap<String, std::shared_ptr<WsServer::Connection>, DefaultHashFunctions, CriticalSection>::Iterator it(connectionMap);
-	while (it.next()) it.getValue()->send(message.toStdString());
+	while (it.next())
+	{
+		it.getValue()->send(message.toStdString());
+	}
 }
 
 void SimpleWebSocketServer::send(const char* data, int numData)
@@ -108,13 +99,16 @@ void SimpleWebSocketServer::send(const char* data, int numData)
 	HashMap<String, std::shared_ptr<WsServer::Connection>, DefaultHashFunctions, CriticalSection>::Iterator it(connectionMap);
 	while (it.next())
 	{
-		it.getValue()->send(out_message, nullptr, 130); //130 = binary
+		it.getValue()->send(out_message, nullptr, 130); // 130 = binary
 	}
 }
 
 void SimpleWebSocketServer::sendTo(const String& message, const String& id)
 {
-	if (connectionMap.contains(id)) connectionMap[id]->send(message.toStdString());
+	if (connectionMap.contains(id))
+	{
+		connectionMap[id]->send(message.toStdString());
+	}
 	else
 	{
 		DBG("Websocket connection not found : " << id);
@@ -124,8 +118,11 @@ void SimpleWebSocketServer::sendTo(const String& message, const String& id)
 void SimpleWebSocketServer::sendTo(const MemoryBlock& data, const String& id)
 {
 	std::shared_ptr<WsServer::OutMessage> out_message = std::make_shared<WsServer::OutMessage>();
-	out_message->write((const char*)data.getData(), data.getSize());
-	if (connectionMap.contains(id)) connectionMap[id]->send(out_message, nullptr, 130); //130 = binary
+	out_message->write((const char*) data.getData(), data.getSize());
+	if (connectionMap.contains(id))
+	{
+		connectionMap[id]->send(out_message, nullptr, 130); // 130 = binary
+	}
 	else
 	{
 		DBG("Websocket connection not found : " << id);
@@ -137,7 +134,10 @@ void SimpleWebSocketServer::sendExclude(const String& message, const StringArray
 	HashMap<String, std::shared_ptr<WsServer::Connection>, DefaultHashFunctions, CriticalSection>::Iterator it(connectionMap);
 	while (it.next())
 	{
-		if (excludeIds.contains(it.getKey())) continue;
+		if (excludeIds.contains(it.getKey()))
+		{
+			continue;
+		}
 		it.getValue()->send(message.toStdString());
 	}
 }
@@ -145,32 +145,43 @@ void SimpleWebSocketServer::sendExclude(const String& message, const StringArray
 void SimpleWebSocketServer::sendExclude(const MemoryBlock& data, const StringArray excludeIds)
 {
 	std::shared_ptr<WsServer::OutMessage> out_message = std::make_shared<WsServer::OutMessage>();
-	out_message->write((const char*)data.getData(), data.getSize());
+	out_message->write((const char*) data.getData(), data.getSize());
 
 	HashMap<String, std::shared_ptr<WsServer::Connection>, DefaultHashFunctions, CriticalSection>::Iterator it(connectionMap);
 	while (it.next())
 	{
-		if (excludeIds.contains(it.getKey())) continue;
-		it.getValue()->send(out_message, nullptr, 130); //130 = binary
+		if (excludeIds.contains(it.getKey()))
+		{
+			continue;
+		}
+		it.getValue()->send(out_message, nullptr, 130); // 130 = binary
 	}
 }
 
 void SimpleWebSocketServer::stopInternal()
 {
-
-	if (ioService != nullptr) ioService->stop();
+	if (ioService != nullptr)
+	{
+		ioService->stop();
+	}
 	ScopedLock lock(serverLock);
 
 	if (ws != nullptr)
 	{
 		std::unordered_set<std::shared_ptr<WsServer::Connection>> connections = ws->get_connections();
-		for (auto& c : connections) c->send_close(1000, "Server destroyed");
+		for (auto& c : connections)
+		{
+			c->send_close(1000, "Server destroyed");
+		}
 		connectionMap.clear();
 
 		ws->stop();
 	}
 
-	if (http != nullptr) http->stop();
+	if (http != nullptr)
+	{
+		http->stop();
+	}
 
 	ws.reset();
 	http.reset();
@@ -181,7 +192,10 @@ void SimpleWebSocketServer::stopInternal()
 
 void SimpleWebSocketServer::closeConnectionInternal(const String& id, int code, const String& reason)
 {
-	if (!connectionMap.contains(id)) return;
+	if (!connectionMap.contains(id))
+	{
+		return;
+	}
 	connectionMap[id]->send_close(code, reason.toStdString());
 }
 
@@ -194,7 +208,10 @@ void SimpleWebSocketServer::initServer()
 		ioService = std::make_shared<asio::io_service>();
 		DBG("HTTP create");
 		http.reset(new HttpServer());
-		if (localAddress.isNotEmpty()) http->config.address = localAddress.toStdString();
+		if (localAddress.isNotEmpty())
+		{
+			http->config.address = localAddress.toStdString();
+		}
 		http->config.port = port;
 		http->io_service = ioService;
 
@@ -206,7 +223,7 @@ void SimpleWebSocketServer::initServer()
 		http->default_resource["PATCH"] = httpCallbackFunc;
 		http->on_upgrade = std::bind(&SimpleWebSocketServer::onHTTPUpgrade, this, std::placeholders::_1, std::placeholders::_2);
 
-		//WebSocket init
+		// WebSocket init
 		DBG("WS create");
 		ws.reset(new WsServer());
 		auto& wsEndpoint = ws->endpoint[("^" + wsSuffix + "/?$").toStdString()];
@@ -215,7 +232,6 @@ void SimpleWebSocketServer::initServer()
 		wsEndpoint.on_error = std::bind(&SimpleWebSocketServer::onErrorCallback, this, std::placeholders::_1, std::placeholders::_2);
 		wsEndpoint.on_open = std::bind(&SimpleWebSocketServer::onNewConnectionCallback, this, std::placeholders::_1);
 		wsEndpoint.on_close = std::bind(&SimpleWebSocketServer::onConnectionCloseCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
-
 
 		http->config.timeout_request = 1;
 		http->config.timeout_content = 300;
@@ -233,7 +249,10 @@ void SimpleWebSocketServer::initServer()
 
 		webSocketListeners.call(&Listener::serverInitSuccess);
 
-		if (ioService != nullptr) ioService->run();
+		if (ioService != nullptr)
+		{
+			ioService->run();
+		}
 	}
 	catch (std::exception e)
 	{
@@ -246,7 +265,6 @@ int SimpleWebSocketServer::getNumActiveConnections() const
 {
 	return connectionMap.size();
 }
-
 
 String SimpleWebSocketServer::getConnectionString(std::shared_ptr<WsServer::Connection> connection) const
 {
@@ -276,9 +294,8 @@ void SimpleWebSocketServerBase::serveFile(const File& file, std::shared_ptr<Http
 		MemoryBlock b;
 		std::unique_ptr<FileInputStream> fs = file.createInputStream();
 		fs->readIntoMemoryBlock(b);
-		response->write((const char*)b.getData(), b.getSize());
+		response->write((const char*) b.getData(), b.getSize());
 	}
-
 }
 
 void SimpleWebSocketServerBase::serveFile(const File& file, std::shared_ptr<HttpsServer::Response> response)
@@ -304,16 +321,17 @@ void SimpleWebSocketServerBase::serveFile(const File& file, std::shared_ptr<Http
 		MemoryBlock b;
 		std::unique_ptr<FileInputStream> fs = file.createInputStream();
 		fs->readIntoMemoryBlock(b);
-		response->write((const char*)b.getData(), b.getSize());
+		response->write((const char*) b.getData(), b.getSize());
 	}
-
 }
-
 
 void SimpleWebSocketServer::onMessageCallback(std::shared_ptr<WsServer::Connection> connection, std::shared_ptr<WsServer::InMessage> in_message)
 {
 	String id = getConnectionString(connection);
-	if (in_message->fin_rsv_opcode == 129) webSocketListeners.call(&Listener::messageReceived, id, String(in_message->string()));
+	if (in_message->fin_rsv_opcode == 129)
+	{
+		webSocketListeners.call(&Listener::messageReceived, id, String(in_message->string()));
+	}
 	else if (in_message->fin_rsv_opcode == 130)
 	{
 		MemoryBlock b(in_message->string().c_str(), in_message->size());
@@ -375,18 +393,32 @@ void SimpleWebSocketServer::httpDefaultCallback(std::shared_ptr<HttpServer::Resp
 
 	if (rootPath.exists() && rootPath.isDirectory())
 	{
-		//String content = "";
+		// String content = "";
 		String contentType = "text/html";
 		File f;
 
 		String path = request->path.substr(1);
-		if (path.isEmpty()) path = "index.html";
-		f = rootPath.getChildFile(path); //substr to remove the first "/"
+		if (path.isEmpty())
+		{
+			path = "index.html";
+		}
+		f = rootPath.getChildFile(path); // substr to remove the first "/"
 
-		if (f.exists() && f.isDirectory()) f = f.getChildFile("index.html");
+		if (f.exists() && f.isDirectory())
+		{
+			f = f.getChildFile("index.html");
+		}
 
 		if (f.existsAsFile())
 		{
+			// check that file is not outside rootPath
+			if (!f.isAChildOf(rootPath))
+			{
+				*response << "HTTP/1.1 403 Forbidden";
+				serveFile(rootPath.getChildFile("403.html"), response);
+				return;
+			}
+
 			serveFile(f, response);
 			return;
 		}
@@ -399,19 +431,11 @@ void SimpleWebSocketServer::httpDefaultCallback(std::shared_ptr<HttpServer::Resp
 	*response << "HTTP/1.1 404 Not Found";
 }
 
-
-
-
-//SECURE
+// SECURE
 
 #if SIMPLEWEB_SECURE_SUPPORTED
 
-SecureWebSocketServer::SecureWebSocketServer(const String& certFile, const String& privateKeyFile, const String& verifyFile) :
-	certFile(certFile),
-	keyFile(privateKeyFile),
-	verifyFile(verifyFile)
-{
-}
+SecureWebSocketServer::SecureWebSocketServer(const String& certFile, const String& privateKeyFile, const String& verifyFile) : certFile(certFile), keyFile(privateKeyFile), verifyFile(verifyFile) {}
 
 SecureWebSocketServer::~SecureWebSocketServer()
 {
@@ -421,7 +445,10 @@ SecureWebSocketServer::~SecureWebSocketServer()
 void SecureWebSocketServer::send(const String& message)
 {
 	HashMap<String, std::shared_ptr<WssServer::Connection>>::Iterator it(connectionMap);
-	while (it.next()) it.getValue()->send(message.toStdString());
+	while (it.next())
+	{
+		it.getValue()->send(message.toStdString());
+	}
 }
 
 void SecureWebSocketServer::send(const char* data, int numData)
@@ -431,13 +458,16 @@ void SecureWebSocketServer::send(const char* data, int numData)
 	HashMap<String, std::shared_ptr<WssServer::Connection>>::Iterator it(connectionMap);
 	while (it.next())
 	{
-		it.getValue()->send(out_message, nullptr, 130); //130 = binary
+		it.getValue()->send(out_message, nullptr, 130); // 130 = binary
 	}
 }
 
 void SecureWebSocketServer::sendTo(const String& message, const String& id)
 {
-	if (connectionMap.contains(id)) connectionMap[id]->send(message.toStdString());
+	if (connectionMap.contains(id))
+	{
+		connectionMap[id]->send(message.toStdString());
+	}
 	else
 	{
 		DBG("[Dashboard] Websocket connection not found : " << id);
@@ -447,8 +477,11 @@ void SecureWebSocketServer::sendTo(const String& message, const String& id)
 void SecureWebSocketServer::sendTo(const MemoryBlock& data, const String& id)
 {
 	std::shared_ptr<WssServer::OutMessage> out_message = std::make_shared<WssServer::OutMessage>();
-	out_message->write((const char*)data.getData(), data.getSize());
-	if (connectionMap.contains(id)) connectionMap[id]->send(out_message, nullptr, 130); //130 = binary
+	out_message->write((const char*) data.getData(), data.getSize());
+	if (connectionMap.contains(id))
+	{
+		connectionMap[id]->send(out_message, nullptr, 130); // 130 = binary
+	}
 	else
 	{
 		DBG("[Dashboard] Websocket connection not found : " << id);
@@ -460,7 +493,10 @@ void SecureWebSocketServer::sendExclude(const String& message, const StringArray
 	HashMap<String, std::shared_ptr<WssServer::Connection>>::Iterator it(connectionMap);
 	while (it.next())
 	{
-		if (excludeIds.contains(it.getKey())) continue;
+		if (excludeIds.contains(it.getKey()))
+		{
+			continue;
+		}
 		it.getValue()->send(message.toStdString());
 	}
 }
@@ -468,32 +504,44 @@ void SecureWebSocketServer::sendExclude(const String& message, const StringArray
 void SecureWebSocketServer::sendExclude(const MemoryBlock& data, const StringArray excludeIds)
 {
 	std::shared_ptr<WssServer::OutMessage> out_message = std::make_shared<WssServer::OutMessage>();
-	out_message->write((const char*)data.getData(), data.getSize());
+	out_message->write((const char*) data.getData(), data.getSize());
 
 	HashMap<String, std::shared_ptr<WssServer::Connection>>::Iterator it(connectionMap);
 	while (it.next())
 	{
-		if (excludeIds.contains(it.getKey())) continue;
-		it.getValue()->send(out_message, nullptr, 130); //130 = binary
+		if (excludeIds.contains(it.getKey()))
+		{
+			continue;
+		}
+		it.getValue()->send(out_message, nullptr, 130); // 130 = binary
 	}
 }
 
 void SecureWebSocketServer::stopInternal()
 {
-	if (ioService != nullptr) ioService->stop();
+	if (ioService != nullptr)
+	{
+		ioService->stop();
+	}
 
 	ScopedLock lock(serverLock);
 
 	if (ws != nullptr)
 	{
 		std::unordered_set<std::shared_ptr<WssServer::Connection>> connections = ws->get_connections();
-		for (auto& c : connections) c->send_close(1000, "Server destroyed");
+		for (auto& c : connections)
+		{
+			c->send_close(1000, "Server destroyed");
+		}
 		connectionMap.clear();
 
 		ws->stop();
 	}
 
-	if (http != nullptr) http->stop();
+	if (http != nullptr)
+	{
+		http->stop();
+	}
 
 	ws.reset();
 	http.reset();
@@ -502,13 +550,15 @@ void SecureWebSocketServer::stopInternal()
 
 void SecureWebSocketServer::closeConnectionInternal(const String& id, int code, const String& reason)
 {
-	if (!connectionMap.contains(id)) return;
+	if (!connectionMap.contains(id))
+	{
+		return;
+	}
 	connectionMap[id]->send_close(code, reason.toStdString());
 }
 
 void SecureWebSocketServer::initServer()
 {
-
 	ScopedLock lock(serverLock);
 
 	try
@@ -522,9 +572,9 @@ void SecureWebSocketServer::initServer()
 		http->default_resource["GET"] = std::bind(&SecureWebSocketServer::httpDefaultCallback, this, std::placeholders::_1, std::placeholders::_2);
 		http->on_upgrade = std::bind(&SecureWebSocketServer::onHTTPUpgrade, this, std::placeholders::_1, std::placeholders::_2);
 
-		//WebSocket init
+		// WebSocket init
 		ws.reset(new WssServer(certFile.toStdString(), keyFile.toStdString(), verifyFile.toStdString()));
-		//ws->config.timeout_idle = 1;
+		// ws->config.timeout_idle = 1;
 		ws->config.timeout_request = 2;
 
 		auto& wsEndpoint = ws->endpoint[("^" + wsSuffix + "/?$").toStdString()];
@@ -545,8 +595,10 @@ void SecureWebSocketServer::initServer()
 		isConnecting = false;
 		webSocketListeners.call(&Listener::serverInitSuccess);
 
-		if (ioService != nullptr) ioService->run();
-
+		if (ioService != nullptr)
+		{
+			ioService->run();
+		}
 	}
 	catch (std::exception e)
 	{
@@ -560,7 +612,6 @@ int SecureWebSocketServer::getNumActiveConnections() const
 	return connectionMap.size();
 }
 
-
 String SecureWebSocketServer::getConnectionString(std::shared_ptr<WssServer::Connection> connection) const
 {
 	return String(connection->remote_endpoint().address().to_string()) + ":" + String(connection->remote_endpoint().port());
@@ -569,8 +620,14 @@ String SecureWebSocketServer::getConnectionString(std::shared_ptr<WssServer::Con
 void SecureWebSocketServer::onMessageCallback(std::shared_ptr<WssServer::Connection> connection, std::shared_ptr<WssServer::InMessage> in_message)
 {
 	String id = getConnectionString(connection);
-	if (in_message->fin_rsv_opcode == 129) webSocketListeners.call(&Listener::messageReceived, id, String(in_message->string()));
-	else if (in_message->fin_rsv_opcode == 130) webSocketListeners.call(&Listener::dataReceived, id, in_message->binary);
+	if (in_message->fin_rsv_opcode == 129)
+	{
+		webSocketListeners.call(&Listener::messageReceived, id, String(in_message->string()));
+	}
+	else if (in_message->fin_rsv_opcode == 130)
+	{
+		webSocketListeners.call(&Listener::dataReceived, id, in_message->binary);
+	}
 	else if (in_message->fin_rsv_opcode == 136)
 	{
 		DBG("Connection ended for " << id);
@@ -627,12 +684,18 @@ void SecureWebSocketServer::httpDefaultCallback(std::shared_ptr<HttpsServer::Res
 
 	if (rootPath.exists() && rootPath.isDirectory())
 	{
-		//String content = "";
+		// String content = "";
 		File f;
 		String path = request->path.substr(1);
-		if (path.isEmpty()) path = "index.html";
-		f = rootPath.getChildFile(path); //substr to remove the first "/"
-		if (f.exists() && f.isDirectory()) f = f.getChildFile("index.html");
+		if (path.isEmpty())
+		{
+			path = "index.html";
+		}
+		f = rootPath.getChildFile(path); // substr to remove the first "/"
+		if (f.exists() && f.isDirectory())
+		{
+			f = f.getChildFile("index.html");
+		}
 
 		if (f.existsAsFile())
 		{
